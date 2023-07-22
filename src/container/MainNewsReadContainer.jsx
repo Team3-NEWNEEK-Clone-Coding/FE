60118ce602cb387b56f8bfcf60348398009b2af1
Haegnim <haegnim@gmail.com>
2023-07-22 18:43:44 +0900

    docs : 메인페이지 컴포넌트 구조 수정

--- src/container/MainNewsReadContainer.jsx
+++ src/container/MainNewsReadContainer.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import NewsCard from '../components/newsCard/NewsCard';
+
+const mockDate = [
+    {
+        title: '우리가 알던 중국 경제가 아냐',
+        img: '/imgs/img1.jpeg',
+        category: '경제',
+        date: '2023.07.21',
+    },
+    {
+        title: '우리가 알던 중국 경제가 아냐',
+        img: '/imgs/img1.jpeg',
+        category: '경제',
+        date: '2023.07.21',
+    },
+];
 
 const MainNewsReadContainer = () => {
-    return <div>MainNewsReadContainer</div>;
+    return (
+        <>
+            <NewsCard newsData={mockDate} />
+        </>
+    );
 };
 
 export default MainNewsReadContainer;
