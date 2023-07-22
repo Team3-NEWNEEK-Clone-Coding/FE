30d4647b62b8b61bee3f067e2494bb1920cef5c6
gyurison7 <orangetree401@gmail.com>
2023-07-22 17:37:27 +0900

    design: 푸터 및 푸터 배너 UI 수정

--- src/layout/footerBanner/FooterBanner.jsx
+++ src/layout/footerBanner/FooterBanner.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-import { FooterBannerWrap, HomeFemaleFooterBanner, FooterStatics } from './FooterBannerStyle';
+import { FooterBannerWrap, FooterBannerLink, FooterBannerCtaArrow, FooterStatics } from './FooterBannerStyle';
 
 const FooterBanner = () => {
   return (
     <FooterBannerWrap>
-      <HomeFemaleFooterBanner>
-        <a href="https://www.notion.so/11e07b3b430a42a9ac8ed26893029e56" target="_blank" rel="noopener noreferrer">
+      <aside className="home-female">
+        <FooterBannerLink href="https://www.notion.so/11e07b3b430a42a9ac8ed26893029e56" target="_blank" rel="noopener noreferrer">
           <p>퀴어 프렌들리한 팀을 위한
             <span className="mobile-block">뉴닉 레인보우 가이드
               <span role="img" aria-label="">🏳️‍🌈</span>
             </span>
           </p>
-          <div className="cta-arrow ">
+          <FooterBannerCtaArrow className="cta-arrow ">
             <div className="line"></div>
-          </div>
-        </a>
-      </HomeFemaleFooterBanner>
-      <HomeFemaleFooterBanner>
-        <a href="https://www.notion.so/90eb61e64b18407196422b3405a3bab6" target="_blank" rel="noopener noreferrer">
+          </FooterBannerCtaArrow>
+        </FooterBannerLink>
+      </aside>
+      <aside className="home-female">
+        <FooterBannerLink href="https://www.notion.so/90eb61e64b18407196422b3405a3bab6" target="_blank" rel="noopener noreferrer">
           <p>3월 8일에 업데이트된
             <span className="mobile-block">뉴닉의 여성용어 가이드
               <span role="img" aria-label="">🍞🌹</span>
             </span>
           </p>
-          <div className="cta-arrow ">
+          <FooterBannerCtaArrow className="cta-arrow ">
             <div className="line"></div>
-          </div>
-        </a>
-      </HomeFemaleFooterBanner>
-      <HomeFemaleFooterBanner>
+          </FooterBannerCtaArrow>
+        </FooterBannerLink>
+      </aside>
+      <aside className="home-female">
         <FooterStatics href="/">
           <p className='footer-banner-text'>
             <span className="mobile-block">오늘까지 <b>588회</b> 뉴스레터를 발행했고&nbsp;</span>
             <b>557,027명</b>이 구독했어요!
           </p>
-          <div className="cta-arrow ">
+          <FooterBannerCtaArrow className="cta-arrow ">
             <div className="line"></div>
-          </div>
+          </FooterBannerCtaArrow>
           <div className="hover-area marquee is-right">
             {
               Array(24)
@@ -45,7 +45,7 @@ const FooterBanner = () => {
             }
           </div>
         </FooterStatics>
-      </HomeFemaleFooterBanner>
+      </aside>
     </FooterBannerWrap>
   )
 }
