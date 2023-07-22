fc03be3cdf29eb7ff008070ff076e2967d10f597
Haegnim <haegnim@gmail.com>
2023-07-22 09:40:34 +0900

    docs : 라우터 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Router from './router/Router';
+import GlobalStyle from './assets/GlobalStyle';
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <Router />
+        </div>
+    );
 }
 
 export default App;
