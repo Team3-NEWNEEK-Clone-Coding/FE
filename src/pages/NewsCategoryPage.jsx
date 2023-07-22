19f80629ed5dad10eed546d51cafb398f8fea164
Haegnim <haegnim@gmail.com>
2023-07-22 18:31:42 +0900

    style : 라우터 페이지 추가

---
+++ src/pages/NewsCategoryPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../layout/NavBar';
+import HomeBanner from '../components/homeBanner/HomeBanner';
+import HeaderBanner from '../components/headerBanner/HeaderBanner';
+import MainNewsReadContainer from '../container/MainNewsReadContainer';
+const NewsCategoryPage = () => {
+    return (
+        <>
+            <NavBar />
+            <HeaderBanner />
+            <div className="center">
+                <MainNewsReadContainer />
+            </div>
+            <button>더보기</button>
+            <HomeBanner />
+        </>
+    );
+};
+
+export default NewsCategoryPage;
