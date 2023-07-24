ac0dfb2707ab0426a361b0a242284a16fe940065
Haegnim <haegnim@gmail.com>
2023-07-24 09:39:29 +0900

    feat : api 연결

--- src/api/news.js
+++ src/api/news.js
@@ -1,1 +1,28 @@
 import axios from 'axios';
+
+export const getAllNews = async ({ currentPage, limit }) => {
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/news`, {
+            params: { page: currentPage, limit: limit },
+        });
+        return response.data;
+    } catch (error) {}
+};
+
+export const getCategoryNews = async ({ currentPage, category }) => {
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/news/`, {
+            params: { page: currentPage, category: category },
+        });
+        return response.data;
+    } catch (error) {}
+};
+
+export const searchNews = async ({ currentPage, keyword }) => {
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/news/search`, {
+            params: { page: currentPage, keyword: keyword },
+        });
+        return response.data;
+    } catch (error) {}
+};
