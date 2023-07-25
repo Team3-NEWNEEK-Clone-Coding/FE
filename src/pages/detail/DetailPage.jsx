5e565404855a2ee81f9d69925ff9d067126dd0c7
Haegnim <haegnim@gmail.com>
2023-07-25 10:46:55 +0900

    fix : 충돌 수정 업로드

--- src/pages/detail/DetailPage.jsx
+++ src/pages/detail/DetailPage.jsx
@@ -4,13 +4,27 @@ import DetailPageLike from '../../components/detailPageLike/DetailPageLike';
 import DetailPageSubscribe from '../../components/detailPageSubscribe/DetailPageSubscribe';
 import HomeBanner from '../../components/homeBanner/HomeBanner';
 import { useParams } from 'react-router';
-import { useQuery } from 'react-query';
-import { getNewsDetail } from '../../api/newsDetail';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { getNewsDetail, postUpdateLike } from '../../api/newsDetail';
 
 const DetailPage = () => {
     const { id } = useParams();
     const { data: post, isLoading, isError } = useQuery(['post', id], () => getNewsDetail(id));
     console.log(post);
+
+    const queryClient = useQueryClient();
+    const mutation = useMutation(postUpdateLike, {
+        onSuccess: (data) => {
+            queryClient.setQueriesData(['post', id], (old) => ({
+                ...old,
+                heart: data.heart,
+            }));
+        },
+    });
+
+    const likeButtonHandler = () => {
+        mutation.mutate(id);
+    }
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -33,15 +47,14 @@ const DetailPage = () => {
                 <PostContainer className="post-container">
                     <div className="post-featured">
                         <img src={post.img} alt="" />
-
                     </div>
                     <div className="post-body" dangerouslySetInnerHTML={{ __html: post.content }} />
                 </PostContainer>
             </div>
             <PostHashtag className="post-hashtag">
-                <a className="post-hashtag-item" href="/search/posts?keyword=사회">#{post.category}</a>
+                <a className="post-hashtag-item" href={`/search/posts?keyword=${post.category}`}>#{post.category}</a>
             </PostHashtag>
-            <DetailPageLike post={post} />
+            <DetailPageLike post={post} likeButtonHandler={likeButtonHandler} />
             <DetailPageSubscribe />
             <HomeBanner />
         </section >
