f5e95ccb13458c6e883a2805c4de224364a4efb9
gyurison7 <orangetree401@gmail.com>
2023-07-25 16:32:56 +0900

    feat: 뉴스레터 구독하기 기능 구현

--- src/components/detailPageSubscribe/DetailPageSubscribe.jsx
+++ src/components/detailPageSubscribe/DetailPageSubscribe.jsx
@@ -1,18 +1,64 @@
+import { useState } from "react";
 import Button from "../common/button/Button";
 import { PostSubscribe } from "./DetailPageSubscribeStyle";
+import { postSub } from "../../api/sub";
 
-export const subscribeFooter = () => {
+export const DetailPageSubscribe = () => {
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [nicknameError, setNicknameError] = useState("");
+
+    const emailOnchangeHadler = (event) => {
+        setEmail(event.target.value);
+        setEmailError("");
+    }
+
+    const nicknameOnchangeHadler = (event) => {
+        setNickname(event.target.value);
+        setNicknameError("");
+    }
+
+    const subscribeClickHandler = async (event) => {
+        event.preventDefault();
+        if (email === '') {
+            setEmailError("이메일 주소를 입력해주세요.");
+            return;
+        }
+        if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) === false) {
+            setEmailError("이메일 형식이 아니에요!");
+            return;
+        }
+        if (nickname === '') {
+            setNicknameError("닉네임을 입력해주세요.");
+            return;
+        }
+
+        const subData = { email, nickname };
+        try {
+            await postSub(subData);
+            alert("구독 신청이 완료되었습니다.");
+            setEmail("");
+            setNickname("");
+        } catch (error) {
+            alert("구독 신청에 실패했습니다. 다시 시도해주세요.");
+            console.error(error);
+        }
+    }
+
     return (
         <PostSubscribe>
             <fieldset className="post-subscribe-group">
                 <div className="textfield post-subscribe-field email">
-                    <input type="text" name="email" className="textfield-input" placeholder="이메일 주소" value="" />
+                    <input type="text" name="email" placeholder="이메일 주소" value={email} onChange={emailOnchangeHadler} />
+                    {emailError && <small className="textfield-helper">{emailError}</small>}
                 </div>
                 <div className="inline-group">
                     <div className="textfield post-subscribe-field nickname">
-                        <input type="text" name="nickname" className="textfield-input" placeholder="닉네임" value="" />
+                        <input type="text" name="nickname" placeholder="닉네임" value={nickname} onChange={nicknameOnchangeHadler} />
+                        {nicknameError && <small className="textfield-helper">{nicknameError}</small>}
                     </div>
-                    <Button size="sm" theme="DetailPageNewsBtn" type="submit" className="post-subscribe-submit">뉴스레터 구독하기</Button>
+                    <Button size="sm" theme="DetailPageNewsBtn" type="submit" onClickEvent={subscribeClickHandler}>뉴스레터 구독하기</Button>
                 </div>
             </fieldset>
             <p className="post-subscribe-terms">구독할 경우
@@ -25,5 +71,5 @@ export const subscribeFooter = () => {
     )
 }
 
-export default subscribeFooter;
+export default DetailPageSubscribe;
 
