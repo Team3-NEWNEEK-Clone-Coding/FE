86a6cb805feea6458104332398edf425af4be179
Haegnim <haegnim@gmail.com>
2023-07-25 20:41:09 +0900

    update : 리액트쿼리 initialData 사용

--- src/hooks/useFetchNews.js
+++ src/hooks/useFetchNews.js
@@ -1,16 +1,37 @@
 // useFetchNews.js
-import { useState, useEffect } from 'react';
-import { useQuery } from 'react-query';
+import { useState, useEffect, useCallback } from 'react';
+// import { useQuery } from 'react-query';
+import { useQueryClient, useQuery } from 'react-query';
 
 const useFetchNews = (queryKey, fetchFunction, dependency) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [newsData, setNewsData] = useState(null);
     const [totalPage, setTotalPage] = useState(1);
 
+    const queryClient = useQueryClient();
+
+    const getInitialData = useCallback(
+        (pageNum, category) => {
+            const allCachedData = [];
+            for (let pageIndex = 1; pageIndex <= pageNum; pageIndex++) {
+                const cachedData = queryClient.getQueryData([queryKey, pageIndex, category]);
+                if (cachedData) {
+                    const dataList = Array.isArray(cachedData.newsList) ? cachedData.newsList : [];
+                    allCachedData.push(...dataList);
+                }
+            }
+            return allCachedData;
+        },
+        [queryClient]
+    );
+
+    const cachedData = getInitialData(currentPage, dependency);
+    console.log(cachedData);
     const { isLoading, isError, refetch } = useQuery(
         [queryKey, currentPage, dependency], // dependency를 추가합니다.
         () => fetchFunction({ currentPage, dependency }),
         {
+            initialData: cachedData,
             onSuccess: (response) => {
                 setNewsData((prevData) =>
                     prevData ? [...prevData, ...response.newsList] : response.newsList
