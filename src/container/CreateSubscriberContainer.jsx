5cb485bbbaf04b7e105cf9f15d66c8389a83c2cb
gyurison7 <orangetree401@gmail.com>
2023-07-25 18:49:38 +0900

    feat: 메인페이지 뉴스레터 구독하기 기능 구현

--- src/container/CreateSubscriberContainer.jsx
+++ src/container/CreateSubscriberContainer.jsx
@@ -1,40 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderBanner from '../components/headerBanner/HeaderBanner';
-import useInput from '../hooks/useInput';
-import useCheckBox from '../hooks/useCheckBox';
 import { postSub } from '../api/sub';
 import { useMutation } from 'react-query';
+
 const CreateSubscriberContainer = () => {
-    const [email, onChangeEmailHandler] = useInput();
-    const [nickname, onChangeNicknameHandler] = useInput();
-    const [checkBox, onChangeCheckBoxHandler] = useCheckBox();
-    const [checkBox2, onChangeCheckBoxHandler2] = useCheckBox();
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [checkbox, setCheckbox] = useState(false);
+    const [checkbox2, setCheckbox2] = useState(false);
+
+    const [emailError, setEmailError] = useState("");
+    const [nicknameError, setNicknameError] = useState("");
+    const [checkboxError, setCheckboxError] = useState("");
+    const [checkboxError2, setCheckboxError2] = useState("");
 
     const mutation = useMutation((subData) => postSub(subData));
 
+    const onChangeEmailHandler = (e) => { 
+        setEmail(e.target.value);
+        setEmailError("");
+    }
+
+    const onChangeNicknameHandler = (e) => {
+        setNickname(e.target.value);
+        setNicknameError("");
+    }
+
+    const onChangeCheckBoxHandler = (e) => {
+        setCheckbox(e.target.checked);
+        setCheckboxError("");
+    }
+    
+    const onChangeCheckBoxHandler2 = (e) => {
+        setCheckbox2(e.target.checked);
+        setCheckboxError2("");
+    }
+
     const onSubmitSubscribeHandler = async (e) => {
         e.preventDefault();
-        if (!email || !nickname) {
-            alert('이메일과 닉네임을 확인해주세요');
+        if (email === '') {
+            setEmailError("이메일 주소를 입력해주세요.");
             return;
         }
-        if (!checkBox || !checkBox2) {
-            alert('개인정보 수집, 이용에 동의해주세요');
+        if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) === false) {
+            setEmailError("이메일 형식이 아니에요!");
             return;
         }
-        const subData = {
-            email,
-            nickname,
-        };
-        await mutation.mutateAsync(subData);
+        if (nickname === '') {
+            setNicknameError("닉네임을 입력해주세요.");
+            return;
+        }
+        if (!checkbox) {
+            setCheckboxError("약관에 동의해주세요.");
+            return;
+        }
+        if (!checkbox2) {
+            setCheckboxError2("약관에 동의해주세요.");
+            return;
+        }
+        const subData = { email, nickname };
+        try {
+            await mutation.mutateAsync(subData);
+            alert("구독 신청이 완료되었습니다.");
+            setEmail("");
+            setNickname("");
+            setCheckbox(false);
+            setCheckbox2(false);
+        } catch (error) {
+            alert("구독 신청에 실패했습니다. 다시 시도해주세요.");
+            console.error(error);
+        }
     };
+
     return (
         <HeaderBanner
             onSubmitEvent={onSubmitSubscribeHandler}
             email={email}
             nickname={nickname}
-            // checkBox={checkBox}
-            // checkBox2={checkBox2}
+            checkbox={checkbox}
+            checkbox2={checkbox2}
+            emailError={emailError}
+            nicknameError={nicknameError}
+            checkboxError={checkboxError}
+            checkboxError2={checkboxError2}
             onChangeEmailHandler={onChangeEmailHandler}
             onChangeNicknameHandler={onChangeNicknameHandler}
             onChangeCheckBoxHandler={onChangeCheckBoxHandler}
