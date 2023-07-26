5aa1b19ed2b87e072dc65d2e5f9b190df4a38c50
gyurison7 <orangetree401@gmail.com>
2023-07-26 17:48:09 +0900

    style: 푸터 수정

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -32,13 +32,12 @@ const Footer = () => {
                 </div>
             </StFooterSitemap>
             <StFooterAddressInfo>
-                ㈜뉴닉 / 대표: 김소연 / 사업자등록번호: 632-81-01159 / 대표전화: 02-6952-1807 / 통신판매번호: 2020-서울마포-2938 / 개인정보보호책임자: 김소연 / 담당자메일주소: whatsup@newneek.co
+                NEWNIQUE / Team3 NEWNEEK Clone Coding / BE: 권능현 김민규 김예진 김정빈 최서림 / FE: 김혜경 손규리 이도영
                 <br />
-                서울특별시 마포구 어울마당로 35, 5층 (04048)
+                <a href="https://github.com/Team3-NEWNEEK-Clone-Coding">https://github.com/Team3-NEWNEEK-Clone-Coding</a>
                 <br />
-                <p>근무시간이 일정하지 않아 전화대신 고객센터를 이용해주세요!</p>
             </StFooterAddressInfo>
-            <StFooterCopyright>ⓒ NEWNEEK Co., Ltd. 2018-2020.</StFooterCopyright>
+            <StFooterCopyright>ⓒ NEWNIQUE Co., Ltd. 2023.</StFooterCopyright>
             <a href="/build" style={{ display: 'none' }}>.</a>
         </StFooter>
     );
