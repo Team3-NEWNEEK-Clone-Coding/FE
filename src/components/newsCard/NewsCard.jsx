1d86b290eb74f63c9cbf06685ad2e9c7298e573d
Haegnim <haegnim@gmail.com>
2023-07-26 21:37:39 +0900

    update : 이미지 최적화, 이미지 크기 지정

--- src/components/newsCard/NewsCard.jsx
+++ src/components/newsCard/NewsCard.jsx
@@ -13,36 +13,49 @@ const NewsCard = ({ newsData, $borderTop }) => {
                     const Emoji = CateEmoji[cateEmojiname];
                     return news.img ? (
                         <NewsCardStyle
-                            to={`/detail/${news.id}`}
                             key={`news${idx}`}
                             className={$borderTop ? 'border-top-style' : ''}
                         >
-                            <NewsCardImgWrap>
-                                <img src={news.img} alt="" />
-                            </NewsCardImgWrap>
-                            <NewsCardBody>
-                                <h2 className="news-title">{news.title}</h2>
-                                <div className="new-info">
-                                    <time>{news.date}</time>
-                                    <i className="category-tag">{news.category}</i>
-                                </div>
-                            </NewsCardBody>
+                            <Link to={`/detail/${news.id}`}>
+                                <NewsCardImgWrap>
+                                    <img
+                                        src={news.img}
+                                        alt="news img"
+                                        width="339"
+                                        height="254"
+                                        loading="lazy"
+                                        decoding="async"
+                                    />
+                                </NewsCardImgWrap>
+                                <NewsCardBody>
+                                    <h2 className="news-title">{news.title}</h2>
+                                    <div className="new-info">
+                                        <time>{news.date}</time>
+                                        <i className="category-tag">{news.category}</i>
+                                    </div>
+                                </NewsCardBody>
+                            </Link>
                         </NewsCardStyle>
                     ) : (
                         <NewsCardStyle
-                            to={'/'}
                             key={`news${idx}`}
                             className={$borderTop ? 'border-top-style' : ''}
                         >
-                            <NewsCardBody>
-                                <Emoji $size={'3rem'} $margin={'0 0 1.5rem'} $display={'block'} />
-                                <h2 className="news-title">{news.title}</h2>
-                                <p className="news-content">어서와요 뉴니커 매주 피자를</p>
-                                <div className="new-info">
-                                    <time>{news.date}</time>
-                                    <i className="category-tag">{news.category}</i>
-                                </div>
-                            </NewsCardBody>
+                            <Link to={`/detail/${news.id}`}>
+                                <NewsCardBody>
+                                    <Emoji
+                                        $size={'3rem'}
+                                        $margin={'0 0 1.5rem'}
+                                        $display={'block'}
+                                    />
+                                    <h2 className="news-title">{news.title}</h2>
+                                    <p className="news-content">어서와요 뉴니커 매주 피자를</p>
+                                    <div className="new-info">
+                                        <time>{news.date}</time>
+                                        <i className="category-tag">{news.category}</i>
+                                    </div>
+                                </NewsCardBody>
+                            </Link>
                         </NewsCardStyle>
                     );
                 })}
