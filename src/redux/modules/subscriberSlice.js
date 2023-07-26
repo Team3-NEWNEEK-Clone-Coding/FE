019af78e28bc41d9fd7710608f953d6b6ab2e549
Haegnim <haegnim@gmail.com>
2023-07-26 15:18:06 +0900

    feat : 총 구독자 수 보여주기

---
+++ src/redux/modules/subscriberSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    subscriber: 0,
+};
+
+const subscriberSlice = createSlice({
+    name: 'subscriber',
+    initialState,
+    reducers: {
+        subscriberSub: (state, action) => {
+            state.subscriber = action.payload;
+        },
+    },
+});
+
+export const { subscriberSub } = subscriberSlice.actions;
+
+export default subscriberSlice.reducer;
