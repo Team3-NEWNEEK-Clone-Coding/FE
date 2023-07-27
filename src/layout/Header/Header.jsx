a2dea845190e647f863f27861d30705257950518
Haegnim <haegnim@gmail.com>
2023-07-27 09:21:22 +0900

    design : css 스타일 수정

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -9,49 +9,42 @@ const Header = ({ ...restProps }) => {
   const { selectedEmoji } = useContext(EmojiContext); // 이모지 컨텍스트에서 선택한 이모지 가져오기
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, []);
-
   return (
     <HeaderStyle {...restProps}>
-      <Link to="/" className="logo-img-wrap">
-        <img src="/imgs/new-neek-logo.png" alt="newneek-logo" width="226" height="32" />
-      </Link>
-      <HeaderIconBox>
-        <HeaderButton to="/search">
-          <Icon.Search />
-        </HeaderButton>
-
-        {isLoggedIn ? (
-          <>
-            <HeaderButton to="/login">
-              {selectedEmoji || <Icon.User />} {/* 선택한 이모지가 있으면 이모지를, 아니면 기본 아이콘을 표시 */}
-            </HeaderButton>
-
-            <HeaderButton
-              to="/login"
-              onClick={() => {
-                localStorage.removeItem("accessToken");
-                localStorage.removeItem("emoji");
-                alert("로그아웃이 되었습니다.");
-                setIsLoggedIn(false);
-              }}
-            >
-              <Icon.Logout />
-            </HeaderButton>
-          </>
-        ) : (
-          <HeaderButton to="/login">
-            <Icon.User />
+      <div className="header-inner">
+        <Link to="/" className="logo-img-wrap">
+          <img src="/imgs/new-neek-logo.png" alt="newneek-logo" width="226" height="32" />
+        </Link>
+        <HeaderIconBox>
+          <HeaderButton to="/search">
+            <Icon.Search />
           </HeaderButton>
-        )}
-      </HeaderIconBox>
+
+          {isLoggedIn ? (
+            <>
+              <HeaderButton to="/login">
+                {selectedEmoji || <Icon.User />} {/* 선택한 이모지가 있으면 이모지를, 아니면 기본 아이콘을 표시 */}
+              </HeaderButton>
+
+              <HeaderButton
+                to="/login"
+                onClick={() => {
+                  localStorage.removeItem("accessToken");
+                  localStorage.removeItem("emoji");
+                  alert("로그아웃이 되었습니다.");
+                  setIsLoggedIn(false);
+                }}
+              >
+                <Icon.Logout />
+              </HeaderButton>
+            </>
+          ) : (
+            <HeaderButton to="/login">
+              <Icon.User />
+            </HeaderButton>
+          )}
+        </HeaderIconBox>
+      </div>
     </HeaderStyle>
   );
 };
